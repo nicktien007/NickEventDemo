82ca372dd59ab2a7036a030e032e841e64e09505
nicktien <trojanhorse0077@gmail.com>
2020-03-26 11:32:21 +0800

    移除無用代碼

--- NickEventDemo/NickEventDemo.Iteration2/Program.cs
+++ NickEventDemo/NickEventDemo.Iteration2/Program.cs
@@ -9,7 +9,6 @@ namespace NickEventDemo.Iteration2
 {
     class Program
     {
-        public static Random Rm = new Random(DateTime.Now.Millisecond);
         static void Main(string[] args)
         {
             ProductFactory factory1 = new ProductFactory();
@@ -27,22 +26,6 @@ namespace NickEventDemo.Iteration2
 
             
             Console.ReadKey();
-        }
-
-        //private static void MyDelegateListener(object sender, EventArgs args)
-        //{
-        //    var foo = Rm.Next(500, 2000);
-        //    Console.WriteLine($"Sleep {foo}");
-        //    Thread.Sleep(Rm.Next(500, 2000));
-        //    Console.WriteLine($"{Thread.CurrentThread.ManagedThreadId}");
-        //}
-
-        private static void MyDelegateListener(object sender, EventArgs args)
-        {
-            var foo = Rm.Next(500, 2000);
-            Console.WriteLine($"Sleep {foo}");
-            Thread.Sleep(Rm.Next(500, 2000));
-            Console.WriteLine($"Thread ID is : {Thread.CurrentThread.ManagedThreadId}");
         }
     }
 }
